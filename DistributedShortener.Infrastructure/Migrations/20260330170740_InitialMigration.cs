2ec2cf65cfe97675c1b4c643750894827ea3bb66
Gaston Cisneros <gastoncisneros97@gmail.com>
2026-03-30 14:19:23 -0300

    Initial Migration

---
+++ DistributedShortener.Infrastructure/Migrations/20260330170740_InitialMigration.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DistributedShortener.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "short_links",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    code = table.Column<string>(type: "character varying(6)", maxLength: 6, nullable: false),
+                    original_url = table.Column<string>(type: "character varying(2048)", maxLength: 2048, nullable: false),
+                    created_by = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    expires_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_short_links", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_short_links_code",
+                table: "short_links",
+                column: "code",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "short_links");
+        }
+    }
+}
